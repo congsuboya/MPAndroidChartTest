bfe012079f9edfc34316699a2baa4074d6bf0452
刘超 <liuchaofeichen@163.com>
2017-12-27 15:58:15 +0800

    初版

---
+++ app/src/main/java/com/testdemo/mpcharttest/StackBarChartActivity.java
@@ -0,0 +1,20 @@
+package com.testdemo.mpcharttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.testdemo.mpcharttest.base.BaseActivity;
+
+/**
+ * Created by chaoye on 2017/12/27.
+ */
+
+public class StackBarChartActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stack_bar_layout);
+    }
+}
---
+++ app/src/main/java/com/testdemo/mpcharttest/base/BaseActivity.java
@@ -0,0 +1,42 @@
+package com.testdemo.mpcharttest.base;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by chaoye on 2017/12/27.
+ */
+
+public class BaseActivity extends Activity {
+    protected String[] mMonths = new String[]{
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"
+    };
+
+    protected String[] mParties = new String[]{
+            "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
+            "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
+            "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+            "Party Y", "Party Z"
+    };
+
+    protected Typeface mTfRegular;
+    protected Typeface mTfLight;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.e("OpenActivity", this.getClass().getName());
+
+        mTfRegular = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+        mTfLight = Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf");
+    }
+
+    protected float getRandom(float range, float startsfrom) {
+        return (float) (Math.random() * range) + startsfrom;
+    }
+
+}
